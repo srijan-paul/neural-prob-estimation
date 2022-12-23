47a281f7ec7137c4eafba47b1a664a018e720067
injuly <srijannr1@gmail.com>
2022-12-23 11:13:13 +0530

    added data generation notebook

--- nn.py
+++ nn.py
@@ -1,6 +1,14 @@
-from math import ceil
 from typing import List, Tuple
 import numpy as np
+
+
+def sigmoid(z: np.ndarray):
+    return 1 / (1 + np.exp(-z))
+
+
+def d_sigmoid(z: np.ndarray):
+    sigmoid_z = sigmoid(z)
+    return sigmoid_z * (1 - sigmoid_z)
 
 
 class Network:
@@ -17,6 +25,7 @@ class Network:
 
         # initialize weights
         # self.weights[0] = weights between first hidden layer and input layer
+        # The input layer does not have any weights
         self.weights = []
         for l in range(1, self.num_layers):
             nrows, ncols = layer_sizes[l], layer_sizes[l - 1]
@@ -25,40 +34,87 @@ class Network:
 
         # initialize biases
         # self.biases[0] = biases for the first hidden layer
+        # Input layer does not have any biases
         self.biases = []
         for n_neurons in layer_sizes[1:]:
             bs = np.random.rand(n_neurons, 1)
             self.biases.append(bs)
 
     def activation(self, z: np.ndarray) -> np.ndarray:
-        return 1 / 1 + np.exp(-z)
+        return sigmoid(z)
+
+    def d_activation(self, z: np.ndarray) -> np.ndarray:
+        return d_sigmoid(z)
 
     def generate_batch(
         xs: np.ndarray, ys: np.ndarray, np: np.ndarray, batch_size: int
     ) -> Tuple[int, int]:
         """Generate a batch for stochastic gradient descent.
-        Calling this on a numpy array will shuffle the array in-place,
+        Calling this on an array will shuffle the array in-place,
         then return the first `batch_size` items in it"""
+
         assert batch_size > 0 and batch_size < len(xs) and len(xs) == len(ys)
         np.random.shuffle(xs)
         return (xs[:batch_size], ys[:batch_size])
 
-    def train(self, xs: np.ndarray, ys: np.ndarray, num_epochs=1000):
-        assert isinstance(num_epochs, int), "#epochs must be an integer"
-        for epoch in range(num_epochs):
-            for (x_, y_) in zip(xs, ys):
-                (x, y) = self.generate_batch(x_, y_)
-                print(x, y)
+    def cost(self, pred: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Quadratic cost function"""
+        assert (
+            pred.shape == y.shape
+        ), "prediction and target must have the same dimensions"
+        return ((pred - y) ** 2) / 2
 
-    def predict(self, x: np.ndarray) -> np.ndarray:
-        a = x  # current activation at layer 0 (input layer)
+    def loss_grad_pred(self, pred: np.ndarray, target: np.ndarray) -> np.ndarray:
+        return target - pred
+
+    def loss_grad_z(self, weighted_sum_l: np.ndarray, loss_grad_a: np.ndarray):
+        """
+        Gradient of the loss with respect to weighted-sum input to the final layer
+
+        Parameters
+        ----------
+        `weighted_sum_l` : Weighted sum input to the final layer
+        `loss_grad_a` : Gradient of the loss w.r.t the final layer activations
+        """
+        return d_sigmoid(weighted_sum_l) * loss_grad_a
+
+    def train(self, xs: np.ndarray, ys: np.ndarray, num_epochs=1000):
+        assert (
+            isinstance(num_epochs, int) and num_epochs > 0
+        ), "#epochs must be a positive integer"
+
+        for _ in range(num_epochs):
+            (x_batch, y_batch) = self.generate_batch(xs, ys)
+            for (train_sample, target) in zip(x_batch, y_batch):
+                prediction, activations, weighted_sums = self.forward_pass(train_sample)
+                # C (cost)
+                loss = self.cost(prediction, target)
+                # dC/da_L
+                loss_grad_a = self.loss_grad_pred(prediction, target)
+                # dC/dz_L
+                loss_grad_zL = self.loss_grad_z(weighted_sums[-1], loss_grad_a)
+
+
+    def forward_pass(self, x: np.ndarray, store_values=True) -> np.ndarray:
+        # activation at layer 0 (input layer) are the features of the training sample
+        # itself.
+        current_activation = x
+        activations = [x]
+        weighted_sums = []
         for l in range(self.num_layers - 1):
             weights_l = self.weights[l]
             biases_l = self.biases[l]
-            z = np.dot(weights_l, a) + biases_l
-            a = self.activation(z)
-        return a
+            z = np.dot(weights_l, current_activation) + biases_l
+            if store_values:
+                weighted_sums.append(z)
+            current_activation = self.activation(z)
+            if store_values:
+                activations.append(current_activation)
+        return current_activation, activations, weighted_sums
+
+    def backprop_losses(self):
+        """
+        """
+        pass
 
 
-nn = Network([3, 2, 2])
-print(nn.predict(np.array([[1], [2], [3]])))
