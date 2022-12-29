8887e893ca5558c857894dccb23f5428215cb458
injuly <srijannr1@gmail.com>
2022-12-29 16:20:50 +0530

    fixed SGD bug in training loop

--- nn.py
+++ nn.py
@@ -1,6 +1,5 @@
 from typing import List, Tuple
 import numpy as np
-import json
 
 
 def sigmoid(z: np.ndarray):
@@ -16,8 +15,8 @@ class Network:
     num_layers: int
     layer_sizes: List[int]
     learn_rate: float
-    weights: np.ndarray
-    biases: np.ndarray
+    weights: List[np.ndarray]
+    biases: List[np.ndarray]
 
     def __init__(self, layer_sizes: List[int], learn_rate=0.1):
         self.num_layers = len(layer_sizes)
@@ -48,28 +47,27 @@ class Network:
     def d_activation(self, z: np.ndarray) -> np.ndarray:
         return d_sigmoid(z)
 
-    def generate_batch(
+    def generate_batches(
         self, xs: np.ndarray, ys: np.ndarray, batch_size: int
-    ) -> Tuple[int, int]:
+    ) -> List[Tuple[np.ndarray, np.ndarray]]:
         """Generate a batch for stochastic gradient descent.
         Calling this on an array will shuffle the array in-place,
         then return the first `batch_size` items in it"""
 
         assert batch_size > 0 and batch_size <= len(xs) and len(xs) == len(ys)
-        np.random.shuffle(xs)
-        return (xs[:batch_size], ys[:batch_size])
+        return [(xs[i:i+batch_size], ys[i:i+batch_size]) for i in range(0, len(xs), batch_size)]
 
-    def cost(self, pred: np.ndarray, target: np.ndarray) -> np.ndarray:
+    def loss(self, pred: np.ndarray, target: np.ndarray) -> np.ndarray:
         """Quadratic cost function"""
         assert (
             pred.shape == target.shape
         ), f"prediction and target must have the same dimensions ({pred.shape}, {target.shape})"
-        return ((target - pred) ** 2) / 2
+        return 0.5 * np.linalg.norm(target - pred) ** 2
 
-    def loss_grad_pred(self, pred: np.ndarray, target: np.ndarray) -> np.ndarray:
+    def loss_gradient_pred(self, pred: np.ndarray, target: np.ndarray) -> np.ndarray:
         return pred - target
 
-    def loss_grad_z(self, weighted_sum_l: np.ndarray, loss_grad_a: np.ndarray):
+    def loss_gradient_z(self, weighted_sum_l: np.ndarray, loss_grad_a: np.ndarray):
         """
         Gradient of the loss with respect to weighted-sum input to the final layer
 
@@ -81,16 +79,56 @@ class Network:
         assert weighted_sum_l.shape == loss_grad_a.shape
         return d_sigmoid(weighted_sum_l) * loss_grad_a
 
-    def update_weights(self, layerwise_losses: np.ndarray, activations: np.ndarray):
+    def calculate_nudges(self, layerwise_losses: np.ndarray, activations: np.ndarray):
+        del_bs = []
+        del_ws = []
+        # print("deltas: ", layerwise_losses, "\n")
         for l in range(self.num_layers - 2, -1, -1):
-            loss_l = layerwise_losses[l]
+            grad_loss_zl = layerwise_losses[l]
             a_l_prev = activations[l]
 
-            del_w_l = self.learn_rate * np.dot(loss_l, np.transpose(a_l_prev))
-            self.weights[l] = self.weights[l] - del_w_l
+            del_w_l = np.dot(grad_loss_zl, a_l_prev.T)
+            del_ws.append(del_w_l)
 
-            del_b_l = self.learn_rate * loss_l
-            self.biases[l] = self.biases[l] - del_b_l
+            del_b_l = grad_loss_zl
+            del_bs.append(del_b_l)
+
+        del_bs.reverse()
+        del_ws.reverse()
+
+        return del_ws, del_bs
+
+    def update_params(self, nabla_ws: List[np.ndarray], nabla_bs: List[np.ndarray], batch_size: int):
+        descent_factor = -self.learn_rate / batch_size
+        self.weights = [w + descent_factor * nw for w,
+                        nw in zip(self.weights, nabla_ws)]
+        self.biases = [b + descent_factor * nb for b,
+                       nb in zip(self.biases, nabla_bs)]
+
+    def process_batch(self, batch: List[Tuple[np.ndarray, np.ndarray]]):
+        batch_size = len(batch[0])
+
+        nabla_ws = [np.zeros(w.shape) for w in self.weights]
+        nabla_bs = [np.zeros(b.shape) for b in self.biases]
+        for train_sample, target in zip(batch[0], batch[1]):
+            # print("processing batch:", train_sample, target, "\n")
+            output, activations, weighted_sums = self.forward_pass(train_sample)
+
+            assert (output.shape == target.shape),\
+                "Network output and target have different dimensions"
+
+            layerwise_loss_grads = \
+                self.backprop_loss_grads(output, weighted_sums, target)
+
+            # print("deltas: ", layerwise_loss_grads, "\n")
+
+            del_nabla_ws, del_nabla_bs = \
+                self.calculate_nudges(layerwise_loss_grads, activations)
+            nabla_ws = [nw + del_nw for nw, del_nw in zip(nabla_ws, del_nabla_ws)]
+            nabla_bs = [nb + del_nb for nb, del_nb in zip(nabla_bs, del_nabla_bs)]
+
+        # print("nablas: ", nabla_ws, nabla_bs, "\n")
+        self.update_params(nabla_ws, nabla_bs, batch_size)
 
     def train(self, xs: np.ndarray, ys: np.ndarray, batch_size: int, num_epochs=1000):
         assert (
@@ -98,19 +136,12 @@ class Network:
         ), "#epochs must be a positive integer"
 
         for _ in range(num_epochs):
-            (x_batch, y_batch) = self.generate_batch(xs, ys, batch_size)
-            avg_loss = 0
-            for (train_sample, target) in zip(x_batch, y_batch):
-                output, activations, weighted_sums = self.forward_pass(
-                    train_sample)
-                assert (
-                    output.shape == target.shape
-                ), "Network output and target have different dimensions"
-                avg_loss += (batch_size / len(xs)) * self.cost(output, target)
-                layerwise_losses = self.backprop_losses(
-                    output, weighted_sums, target)
-                self.update_weights(layerwise_losses, activations)
-            print(f"Epoch #{_ + 1}: loss = {avg_loss}")
+            # print(f"-------\nepoch #{_}\n-------")
+            batches = self.generate_batches(xs, ys, batch_size)
+            # print("batches:", [list(zip(xs, ys)) for (xs, ys) in batches], "\n")
+            for batch in batches:
+                self.process_batch(batch)
+            # print(f"params after epoch {_}: {self.weights}, {self.biases}")
 
     def forward_pass(self, x: np.ndarray, store_values=True) -> np.ndarray:
         """
@@ -134,10 +165,8 @@ class Network:
         current_activation = x
         activations = [x]
         weighted_sums = []
-        for l in range(self.num_layers - 1):
-            weights_l = self.weights[l]
-            biases_l = self.biases[l]
-            z = np.dot(weights_l, current_activation) + biases_l
+        for (w, b) in zip(self.weights, self.biases):
+            z = np.dot(w, current_activation) + b
 
             assert z.shape[1] == 1
 
@@ -153,7 +182,7 @@ class Network:
     def predict(self, x: np.ndarray) -> np.ndarray:
         return self.forward_pass(x, False)
 
-    def backprop_losses(
+    def backprop_loss_grads(
         self,
         prediction: np.ndarray,
         weighted_sums: List[np.ndarray],
@@ -166,25 +195,26 @@ class Network:
         Parameters
         ----------
         `loss`: Output of the cost function based on the network's prediction
-        `prediction`: Prediction made by the neural network.
+        `prediction`: Prediction made by the neural network for a training sample.
         `weighted_sums`: A list where the `lth` element is an np.ndarray representing z_l (weighted sum input to neurons at layer l).
-        `activations`: A list where the `lth` element is an np.ndarray representing a_l (activation values of inputs to neurons at layer l).
         `target`: Target output
         """
-        loss_grad_a = self.loss_grad_pred(prediction, target)
+        loss_grad_a = self.loss_gradient_pred(prediction, target)
+        # print("∇C_a_L", loss_grad_a)
 
         # calculate gradient of loss w.r.t weighted-sum input of the current layer.
         # We start with the final layer as the current layer and work backwards.
-        loss_grad_zl = self.loss_grad_z(weighted_sums[-1], loss_grad_a)
+        loss_grad_zl = self.loss_gradient_z(weighted_sums[-1], loss_grad_a)
+        # print("delta_L", loss_grad_zl)
 
         losses = [loss_grad_zl]
         # Now, we go backwards from layer L to layer 1, and
         # calculate the loss vector for every layer.
         for l in range(self.num_layers - 3, -1, -1):
-            w_lnext = self.weights[l + 1]
-            z_l = weighted_sums[l]
+            w_lnext = self.weights[l + 1]  # weight matrix of the next layer
+            z_l = weighted_sums[l]  # z vector for the current layer
             loss_grad_zl = self.d_activation(z_l) * np.dot(
-                np.transpose(w_lnext),
+                w_lnext.T,
                 loss_grad_zl)
             losses.append(loss_grad_zl)
 
@@ -193,11 +223,12 @@ class Network:
 
 
 domain = np.linspace(-0.5, 0.5, 500)
+# domain = [1, 2]
 train_xs = np.array([[[i]] for i in domain])
 train_ys = np.array([[[i * 2]] for i in domain])
 
-# TODO: Using [1, 2, 3, 1] as the layer sizes crashes the training loop.
-# Investigate why
-nn = Network([1, 2, 1], 1)
-nn.train(train_xs, train_ys, 100, 1000)
+nn = Network([1, 1, 1], 1)
+nn.train(train_xs, train_ys, batch_size=50, num_epochs=1000)
 print(nn.predict(np.array([[0.2]])))
+print(nn.predict(np.array([[0.5]])))
+print(nn.predict(np.array([[0.35]])))
